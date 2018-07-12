14d5a6229aa4b00c600ce65390ad39554ea58f11
Adam Gotterer <agotterer@gmail.com>
2018-07-11 23:01:56 -0400

    feat: added Kremal for easier routing. Refactored the handlers to be more manageable.

--- chromie/src/chromie.cr
+++ chromie/src/chromie.cr
@@ -1,6 +1,8 @@
 require "http"
 require "logger"
 
+require "kemal"
+
 require "./chromie/*"
 
 module Chromie
@@ -11,42 +13,44 @@ module Chromie
       set, using default ports 9222-9322"
   end
 
-  class WebSocketHandler < HTTP::WebSocketHandler
-    def initialize
-      super do |socket, context|
-        handler(socket, context)
-      end
+  ws "/browser" do |client_socket|
+    begin
+      port_range = ENV["CHROMIE_CHROME_PORT_START"].to_i..ENV["CHROMIE_CHROME_PORT_END"].to_i
+      chrome_proxy = ChromeProxy.new(client_socket, port_range)
+    rescue ex : ChromeProcessError
+      logger.warn(ex.message)
+      client_socket.close("Chrome process failed to start")
+      next false
     end
 
-    def handler(client_socket : HTTP::WebSocket, context : HTTP::Server::Context)
+    upstream_proxy = UpstreamProxy.new(socket: client_socket, upstream_socket: chrome_proxy.socket)
+            
+    spawn do
       begin
-        port_range = ENV["CHROMIE_CHROME_PORT_START"].to_i..ENV["CHROMIE_CHROME_PORT_END"].to_i
-        chrome_proxy = ChromeProxy.new(client_socket, port_range)
-      rescue ex : ChromeProcessError
-        logger.warn(ex.message)
-        client_socket.close("Chrome process failed to start")
-        return false
+        chrome_proxy.run
+      rescue Errno | IO::Error
+        logger.debug "chrome_proxy#run threw and exception. Closing sockets."
+        upstream_proxy.close("closed") unless upstream_proxy.closed?
+        chrome_proxy.close("closed") unless chrome_proxy.closed?
+        chrome_proxy.kill
       end
+    end
+  end
 
-      upstream_proxy = UpstreamProxy.new(socket: client_socket, upstream_socket: chrome_proxy.socket)
-              
-      spawn do
-        begin
-          chrome_proxy.run
-        rescue Errno | IO::Error
-          logger.debug "chrome_proxy#run threw and exception. Closing sockets."
-          upstream_proxy.close("closed") unless upstream_proxy.closed?
-          chrome_proxy.close("closed") unless chrome_proxy.closed?
-          chrome_proxy.kill
-        end
-      end
+  get "/health" do
+    "ok"
+  end
+
+  class RouteHandler
+    include HTTP::Handler
+
+    def call(context : HTTP::Server::Context)
+      context.request.inspect
     end
   end
 
   def self.run
     port = ENV.fetch("CHROMIE_PORT", "9333")
-    server = HTTP::Server.new(WebSocketHandler.new)
-    puts "Listening on http://0.0.0.0:#{port}"
-    server.listen("0.0.0.0", port.to_i32)
+    Kemal.run(port.to_i32)
   end
 end
